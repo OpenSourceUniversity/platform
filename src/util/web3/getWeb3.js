b96e86cc6264b379031758bf629329e79935b697
Jordan Jambazov <jordan.jambazov@era.io>
2018-02-07 18:49:00 +0200

    Resolve eslint errors in business pages & web3

--- src/util/web3/getWeb3.js
+++ src/util/web3/getWeb3.js
@@ -1,49 +1,33 @@
-import store from '../../store';
 import Web3 from 'web3';
+import store from '../../store';
 
 export const WEB3_INITIALIZED = 'WEB3_INITIALIZED';
 function web3Initialized(results) {
   return {
     type: WEB3_INITIALIZED,
-    payload: results
+    payload: results,
   };
-};
+}
 
-let getWeb3 = new Promise(function(resolve, reject) {
+const getWeb3 = new Promise(((resolve, reject) => { // eslint-disable-line no-unused-vars
   // Wait for loading completion to avoid race conditions with web3 injection timing.
-  window.addEventListener('load', function(dispatch) {
-    var results;
-    var web3 = window.web3;
+  window.addEventListener('load', (dispatch) => { // eslint-disable-line no-unused-vars
+    let web3 = { window };
 
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined' && false) {
-      // Use Mist/MetaMask's provider.
-      web3 = new Web3(web3.currentProvider);
+    // Fallback to localhost if no web3 injection. We've configured this to
+    // use the development console's port by default.
+    const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
 
-      results = {
-        web3Instance: web3
-      };
+    web3 = new Web3(provider);
 
-      console.log('Injected web3 detected.');
+    const results = {
+      web3Instance: web3,
+    };
 
-      resolve(store.dispatch(web3Initialized(results)));
-    } else {
+    console.log('No web3 instance injected, using Local web3.');
 
-      // Fallback to localhost if no web3 injection. We've configured this to
-      // use the development console's port by default.
-      var provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
-
-      web3 = new Web3(provider);
-
-      results = {
-        web3Instance: web3
-      };
-
-      console.log('No web3 instance injected, using Local web3.');
-
-      resolve(store.dispatch(web3Initialized(results)));
-    }
+    resolve(store.dispatch(web3Initialized(results)));
   });
-});
+}));
 
 export default getWeb3;

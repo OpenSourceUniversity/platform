11cd711b11b7276950c8328333380f4be813780f
Jordan Jambazov <jordan.jambazov@era.io>
2018-02-21 13:19:05 +0200

    Remove .call() when adding certificate

--- test/unit/courses/TestCourse.js
+++ test/unit/courses/TestCourse.js
@@ -2,44 +2,37 @@
 
 const Course = artifacts.require("./courses/Course.sol");
 
-
 contract('TestCourse', (accounts) => {
-
   const owner = web3.eth.accounts[0];
   const osu = web3.eth.accounts[1];
   var courseInstance;
 
   it('Deployment', function() {
-    console.log(web3.eth.blockNumber);
     return Course.deployed(owner, osu).then(function(instance) {
       courseInstance = instance;
     });
   });
 
   it('Initial information: owner', () => {
-    console.log(web3.eth.blockNumber);
     return courseInstance.owner.call().then((result) => {
       assert.equal(result, owner);
     });
   });
 
   it('Initial information: !owner', () => {
-    console.log(web3.eth.blockNumber);
     return courseInstance.owner.call().then((result) => {
       assert.notEqual('0xsomeothervaluebutnottherightone', owner);
     });
   });
 
   it('Initial information: osu', () => {
-    console.log(web3.eth.blockNumber);
     return courseInstance.osu.call().then((result) => {
       assert.equal(result, osu);
     });
   });
 
   it('Test adding a course', () => {
-    console.log(web3.eth.blockNumber);
-    return courseInstance.addCourse.call(
+    return courseInstance.addCourse(
       '0x0000000000000000000000000000000000000000',  // Verifier
       ['Python', 'Yoga'],
       5,
@@ -49,7 +42,11 @@ contract('TestCourse', (accounts) => {
       ['0x0000000000000000000000000000000000000002'],
       'Category',
       'Subcategory'
-    ).then((result) => {
+    );
+  });
+
+  it('Test length correctly retrieved', () => {
+    return courseInstance.getLength.call().then((result) => {
       assert.equal(result, 1);
     });
   });

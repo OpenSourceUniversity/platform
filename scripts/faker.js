d76278641502913fb2fd4b56fe80c5d25d8c6978
Jordan Jambazov <jordan.jambazov@era.io>
2018-02-25 14:51:26 +0200

    Update faker to use verified flag

--- scripts/faker.js
+++ scripts/faker.js
@@ -4,14 +4,15 @@ const CertificateStorage = artifacts.require('CertificateStorage');
 function fakeCertificates() {
   CertificateStorage.deployed().then((instance) => {
     // Add certificates
-    Array(50).fill().map((_, i) => {
+    Array(100).fill().map((_, i) => {
       console.log(`Adding certificate ${i}`);
       return instance.addCertificate(
         '0x0000000000000000000000000000000000000001',
         '0x0000000000000000000000000000000000000002',
         '0x0000000000000000000000000000000000000003',
-        ['Python', ' Programming'],
+        ['Test ', ' Test'],
         ['Computer', ' Science'],
+        true,
         10,
         1519302362,
       );

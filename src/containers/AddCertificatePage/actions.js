eafb01c45da007d78fd9e278c3b4e629801ae739
Jordan Jambazov <jordan.jambazov@era.io>
2018-03-08 14:45:50 +0200

    Show web3 errors when adding certificates

--- src/containers/AddCertificatePage/actions.js
+++ src/containers/AddCertificatePage/actions.js
@@ -4,29 +4,45 @@ import store from '../../store';
 const contract = require('truffle-contract');
 
 
-function addCertificate() {
-  const web3 = store.getState().web3.web3Instance;
-  const certificateStorage = contract(CertificateStorage);
-  certificateStorage.setProvider(web3.currentProvider);
-
-  web3.eth.getCoinbase((error, coinbase) => {
-    if (error) {
-      return;
-    }
-
-    certificateStorage.deployed().then((instance) => {
-      const certificateStorageInstance = instance;
-
-      certificateStorageInstance.addCertificate('', '', '', '', '', '', '', { from: coinbase })
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((addCertificateError) => {
-          console.error(addCertificateError);
-        });
+export function addCertificate(state) {
+  return function action(dispatch) {
+    dispatch({
+      type: 'ADD_CERTIFICATE_REQUEST',
     });
-  });
+
+    const web3 = store.getState().web3.web3Instance;
+    const certificateStorage = contract(CertificateStorage);
+    certificateStorage.setProvider(web3.currentProvider);
+
+    web3.eth.getCoinbase((error, coinbase) => {
+      if (error) {
+        return;
+      }
+
+      certificateStorage.deployed().then((instance) => {
+        const certificateStorageInstance = instance;
+
+        certificateStorageInstance.addCertificate(
+          '0x0000000000000000000000000000000000000001',
+          '0x0000000000000000000000000000000000000002',
+          '0x0000000000000000000000000000000000000003',
+          [web3.fromUtf8('Баба '), web3.fromUtf8(' Дядо')],
+          [web3.fromUtf8('Тест '), web3.fromUtf8(' Тест')],
+          true,
+          10,
+          1519302362,
+          { from: coinbase },
+        )
+          .then((result) => {
+            console.log(result);
+          })
+          .catch((addError) => {
+            dispatch({
+              type: 'ADD_CERTIFICATE_FAILURE',
+              error: addError,
+            });
+          });
+      });
+    });
+  };
 }
-
-
-export { addCertificate };
---
+++ src/containers/AddCertificatePage/addCertificateReducer.js
@@ -0,0 +1,23 @@
+const INITIAL_STATE = {
+  isAdding: false,
+};
+
+export default function addCertificateReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'ADD_CERTIFICATE_REQUEST':
+    return Object.assign({}, state, {
+      isAdding: true,
+    });
+  case 'ADD_CERTIFICATE_SUCCESS':
+    return Object.assign({}, state, {
+      isAdding: false,
+    });
+  case 'ADD_CERTIFICATE_FAILURE':
+    return Object.assign({}, state, {
+      isAdding: false,
+      error: action.error.message,
+    });
+  default:
+    return state;
+  }
+}

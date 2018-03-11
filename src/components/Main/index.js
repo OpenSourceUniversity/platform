cd0712b176d8f508cfe29c7f29afd2a7a3f55ea1
Jordan Jambazov <jordan.jambazov@era.io>
2018-03-11 19:16:14 +0200

    Use browser web3 provider & show coinbase

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import HomePage from 'containers/HomePage';
 import CertificatesPage from 'containers/CertificatesPage';
 import AddCertificatePage from 'containers/AddCertificatePage';
@@ -9,25 +8,22 @@ import CoursesPage from 'containers/CoursesPage';
 import JobsPage from 'containers/JobsPage';
 import BusinessPage from 'containers/BusinessPage';
 import ProfilePage from 'containers/ProfilePage';
-import store from '../../store';
 
 class Main extends React.Component {
   state = {}
 
   render() {
     return (
-      <Provider store={store}>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/certificates" component={CertificatesPage} />
-          <Route exact path="/certificates/add" component={AddCertificatePage} />
-          <Route exact path="/businesses/add" component={AddPositionToBusinessPage} />
-          <Route path="/courses" component={CoursesPage} />
-          <Route path="/jobs" component={JobsPage} />
-          <Route path="/business" component={BusinessPage} />
-          <Route path="/profile" component={ProfilePage} />
-        </Switch>
-      </Provider>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/certificates" component={CertificatesPage} />
+        <Route exact path="/certificates/add" component={AddCertificatePage} />
+        <Route exact path="/businesses/add" component={AddPositionToBusinessPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/jobs" component={JobsPage} />
+        <Route path="/business" component={BusinessPage} />
+        <Route path="/profile" component={ProfilePage} />
+      </Switch>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import React from 'react';
 import App from './app';
 import './styles/global.scss';
+// Redux Store
+import store from './store';
 
 const MOUNT_NODE = document.getElementById('root');
 
 render(
   (
     <HashRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </HashRouter>
   ), MOUNT_NODE,
 );

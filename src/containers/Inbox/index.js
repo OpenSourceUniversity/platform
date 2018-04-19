879658f07a5c32dbf99c2a08eb5c94ca81143446
Momchil Jambazov <momchil.jambazov@codeideo.com>
2018-04-19 16:47:22 +0300

    Messages Updates

--- src/containers/Inbox/index.js
+++ src/containers/Inbox/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Button, Label, Input, Menu, Segment, Grid, Comment, Item, Divider, Card, Icon } from 'semantic-ui-react'
+import { Container, Button, Label, Input, Menu, Segment, Grid, Comment, Item, Divider, Message, Card, Icon, Header } from 'semantic-ui-react'
 
 const description = [
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet ante a sem eleifend bibendum. Aliquam ex lectus, volutpat et est nec, luctus cursus dui. In molestie dolor malesuada, varius sapien commodo, iaculis nulla. Nulla hendrerit nisi non nibh blandit, nec sollicitudin augue semper. In nulla arcu, lacinia ac eleifend non, venenatis ut nunc. Suspendisse accumsan libero a leo pretium porttitor. Praesent ornare, odio a eleifend egestas, mi felis ultrices sapien, a tincidunt nibh tortor sit amet mi. Nunc vehicula mi non pharetra ultricies. In finibus tempus sodales. Sed eu orci et mauris malesuada egestas. Sed maximus est eget ipsum condimentum consectetur. Curabitur gravida scelerisque posuere. Proin in commodo massa, eu fringilla libero. Sed posuere ultricies neque, quis vulputate erat commodo sit amet. Pellentesque at sem et massa ultrices lacinia. Suspendisse in dolor vel turpis mollis ornare. Aliquam tempus, justo nec mattis blandit, eros tellus semper leo, sed euismod nisi tellus in metus. Duis ipsum urna, dapibus molestie enim et, bibendum iaculis elit. Quisque molestie dignissim vehicula. Morbi eleifend ligula quam, in sodales felis convallis id. In iaculis porta tincidunt. Nunc imperdiet dui ut congue fringilla. Mauris interdum diam nec erat ultrices, ac condimentum velit ultricies.'
@@ -12,12 +12,20 @@ export default class Inbox extends React.Component {
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
+    let messages = require('../../icons/nav_messages.svg');
     let avatar_placeholder = require('../../icons/avatar_placeholder.svg');
 
     const { activeItem } = this.state
 
     return (
       <Container fluid className='inbox'>
+        <Header size="huge">
+          <svg width='32' height='32' className='icon'> 
+            <image href={messages}  x='0' y='0' width='100%' height='100%'></image>
+          </svg>
+          Messages
+        </Header>
+        <Divider clearing />
         <Menu stackable size='huge'>
           <Menu.Item>
             <Button primary>
@@ -69,126 +77,137 @@ export default class Inbox extends React.Component {
         <Segment>
           <Grid relaxed>
             <Grid.Column width={4}>
-              <Comment.Group threaded>
-                <Item.Group link>
-                  <Item>
-                    <Comment>
-                      <Comment.Avatar as='a' src={avatar_placeholder} />
-                      <Comment.Content>
-                        <Comment.Author as='a'>Matt</Comment.Author>
-                        <Comment.Metadata>
-                          <span>Today at 5:42PM</span> 
-                        </Comment.Metadata>
-                        <Comment.Text>
-                          <Label circular color='orange' size='tiny' empty />
-                           How artistic!
-                        </Comment.Text>
-                      </Comment.Content>
-                    </Comment>
-                  </Item>
-                  <Divider />
-                  <Item>
-                    <Comment>
-                      <Comment.Avatar as='a' src={avatar_placeholder} />
-                      <Comment.Content>
-                        <Comment.Author as='a'>Matt</Comment.Author>
-                        <Comment.Metadata>
-                          <span>Today at 5:42PM</span>
-                        </Comment.Metadata>
-                        <Comment.Text>How artistic!</Comment.Text>
-                      </Comment.Content>
-                    </Comment>
-                  </Item>
-                  <Divider />
-                  <Item>
-                    <Comment>
-                      <Comment.Avatar as='a' src={avatar_placeholder} />
-                      <Comment.Content>
-                        <Comment.Author as='a'>Matt</Comment.Author>
-                        <Comment.Metadata>
-                          <span>Today at 5:42PM</span>
-                        </Comment.Metadata>
-                        <Comment.Text>How artistic!</Comment.Text>
-                      </Comment.Content>
-                    </Comment>
-                  </Item>
-                  <Divider />
-                  <Item>
-                    <Comment>
-                      <Comment.Avatar as='a' src={avatar_placeholder} />
-                      <Comment.Content>
-                        <Comment.Author as='a'>Matt</Comment.Author>
-                        <Comment.Metadata>
-                          <span>Today at 5:42PM</span>
-                        </Comment.Metadata>
-                        <Comment.Text>How artistic!</Comment.Text>
-                      </Comment.Content>
-                    </Comment>
-                  </Item>
-                  <Divider />
-                  <Item>
-                    <Comment>
-                      <Comment.Avatar as='a' src={avatar_placeholder} />
-                      <Comment.Content>
-                        <Comment.Author as='a'>Matt</Comment.Author>
-                        <Comment.Metadata>
-                          <span>Today at 5:42PM</span>
-                        </Comment.Metadata>
-                        <Comment.Text>How artistic!</Comment.Text>
-                      </Comment.Content>
-                    </Comment>
-                  </Item>
-                  <Divider />
-                  <Item>
-                    <Comment>
-                      <Comment.Avatar as='a' src={avatar_placeholder} />
-                      <Comment.Content>
-                        <Comment.Author as='a'>Matt</Comment.Author>
-                        <Comment.Metadata>
-                          <span>Today at 5:42PM</span>
-                        </Comment.Metadata>
-                        <Comment.Text>How artistic!</Comment.Text>
-                      </Comment.Content>
-                    </Comment>
-                  </Item>
+              <Segment>
+                <Comment.Group threaded>
+                  <Item.Group link>
+                    <Item>
+                      <Comment>
+                        <Comment.Avatar as='a' src={avatar_placeholder} />
+                        <Comment.Content>
+                          <Comment.Author as='a'>Matt</Comment.Author>
+                          <Comment.Metadata>
+                            <span>Today at 5:42PM</span> 
+                          </Comment.Metadata>
+                          <Comment.Text>
+                            <Label circular color='orange' size='tiny' empty />
+                            How artistic!
+                          </Comment.Text>
+                        </Comment.Content>
+                      </Comment>
+                    </Item>
+                    <Divider />
+                    <Item>
+                      <Comment>
+                        <Comment.Avatar as='a' src={avatar_placeholder} />
+                        <Comment.Content>
+                          <Comment.Author as='a'>Matt</Comment.Author>
+                          <Comment.Metadata>
+                            <span>Today at 5:42PM</span>
+                          </Comment.Metadata>
+                          <Comment.Text>How artistic!</Comment.Text>
+                        </Comment.Content>
+                      </Comment>
+                    </Item>
+                    <Divider />
+                    <Item>
+                      <Comment>
+                        <Comment.Avatar as='a' src={avatar_placeholder} />
+                        <Comment.Content>
+                          <Comment.Author as='a'>Matt</Comment.Author>
+                          <Comment.Metadata>
+                            <span>Today at 5:42PM</span>
+                          </Comment.Metadata>
+                          <Comment.Text>How artistic!</Comment.Text>
+                        </Comment.Content>
+                      </Comment>
+                    </Item>
+                    <Divider />
+                    <Item>
+                      <Comment>
+                        <Comment.Avatar as='a' src={avatar_placeholder} />
+                        <Comment.Content>
+                          <Comment.Author as='a'>Matt</Comment.Author>
+                          <Comment.Metadata>
+                            <span>Today at 5:42PM</span>
+                          </Comment.Metadata>
+                          <Comment.Text>How artistic!</Comment.Text>
+                        </Comment.Content>
+                      </Comment>
+                    </Item>
+                    <Divider />
+                    <Item>
+                      <Comment>
+                        <Comment.Avatar as='a' src={avatar_placeholder} />
+                        <Comment.Content>
+                          <Comment.Author as='a'>Matt</Comment.Author>
+                          <Comment.Metadata>
+                            <span>Today at 5:42PM</span>
+                          </Comment.Metadata>
+                          <Comment.Text>How artistic!</Comment.Text>
+                        </Comment.Content>
+                      </Comment>
+                    </Item>
+                    <Divider />
+                    <Item>
+                      <Comment>
+                        <Comment.Avatar as='a' src={avatar_placeholder} />
+                        <Comment.Content>
+                          <Comment.Author as='a'>Matt</Comment.Author>
+                          <Comment.Metadata>
+                            <span>Today at 5:42PM</span>
+                          </Comment.Metadata>
+                          <Comment.Text>How artistic!</Comment.Text>
+                        </Comment.Content>
+                      </Comment>
+                    </Item>
 
-                </Item.Group>
-              </Comment.Group>
+                  </Item.Group>
+                </Comment.Group>
+              </Segment>
             </Grid.Column>
             <Grid.Column width={12}>
-              <Card fluid>
-                <Card.Content header='How artistic!' />
-                <Card.Content description={description} />
-                <Card.Content extra>
-                  
-                  
-                  <Menu secondary size='small'>
-                    <Menu.Item>
-                      <Comment.Group>
-                        <Comment>
-                          <Comment.Avatar as='a' src={avatar_placeholder} />
-                          <Comment.Content>
-                            <Comment.Author as='a'>Matt</Comment.Author>
-                            <Comment.Text>
-                              <Comment.Metadata>
-                                <span>Today at 5:42PM</span>
-                              </Comment.Metadata>
-                            </Comment.Text>
-                          </Comment.Content>
-                        </Comment>
-                      </Comment.Group>
-                    </Menu.Item>
+              <Message attached="top">
+                <Menu secondary size='small'>
+                  <Menu.Item>
+                    <Comment.Group>
+                      <Comment>
+                        <Comment.Avatar as='a' src={avatar_placeholder} />
+                        <Comment.Content>
+                          <Comment.Author as='a'>Matt</Comment.Author>
+                          <Comment.Text>
+                            <span>Today at 5:42PM</span>
+                          </Comment.Text>
+                        </Comment.Content>
+                      </Comment>
+                    </Comment.Group>
+                  </Menu.Item>
 
-                    <Menu.Item position='right'>
-                      <Button>
-                        <Icon name='reply' size='small' />          
+                  <Menu.Item position='right'>
+                    <Button>
+                      <Icon name='reply' size='small' />          
                         Reply
-                      </Button>
-                    </Menu.Item>
+                    </Button>
+                    <Button basic>
+                      <Icon name='trash' size='small' />          
+                        Delete
+                    </Button>
+                  </Menu.Item>
 
-                  </Menu>
-                </Card.Content>
-              </Card>
+                </Menu>
+              </Message>
+                
+              <Segment attached padded='very'>
+                <Header as='h3'>
+                  How artistic!
+                </Header>
+                <Divider />
+                {description}
+              </Segment>
+              <Message warning attached='bottom'>
+                <Icon name='warning' />
+                You've reached the end of this content segment!
+              </Message>
+
             </Grid.Column>
           </Grid>
         </Segment>

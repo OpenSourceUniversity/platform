896c0259870eaf57dffda5f5331b3256d1b80ef2
Dobromir Kovachev <dobromir.kovachev@os.university>
2018-05-29 03:11:29 +0300

    Store static data related to languages, industries and job types

---
+++ src/data/jobParameters.js
@@ -0,0 +1,17 @@
+export default {
+  ProficiencyLevel: [
+    { value: 1, text: 'Basic knowledge' },
+    { value: 2, text: 'Novice (limited experience)' },
+    { value: 3, text: 'Intermediate (practical application)' },
+    { value: 4, text: 'Advanced (applied theory)' },
+    { value: 5, text: 'Expert (recognized authority)' },
+  ],
+  JobType: [
+    { value: 1, text: 'Part-time' },
+    { value: 2, text: 'Full-time' },
+    { value: 3, text: 'Temporary' },
+    { value: 4, text: 'Contract' },
+    { value: 5, text: 'Hourly' },
+    { value: 6, text: 'Other' },
+  ],
+};
--- src/data/languagesList.js
+++ src/data/languagesList.js
@@ -1,559 +1,559 @@
 export default {
   Languages: [
     {
-      key: 'abk', text: 'Abkhazian',
+      value: 'abk', text: 'Abkhazian',
     },
     {
-      key: 'aar', text: '	Afar',
+      value: 'aar', text: '	Afar',
     },
     {
-      key: 'afr', text: 'Afrikaans',
+      value: 'afr', text: 'Afrikaans',
     },
     {
-      key: 'aka', text: 'Akan',
+      value: 'aka', text: 'Akan',
     },
     {
-      key: 'alb', text: 'Albanian',
+      value: 'alb', text: 'Albanian',
     },
     {
-      key: 'amh', text: 'Amharic',
+      value: 'amh', text: 'Amharic',
     },
     {
-      key: 'ara', text: 'Arabic',
+      value: 'ara', text: 'Arabic',
     },
     {
-      key: 'arg', text: 'Aragonese',
+      value: 'arg', text: 'Aragonese',
     },
     {
-      key: 'hye', text: 'Armenian',
+      value: 'hye', text: 'Armenian',
     },
     {
-      key: 'asm', text: 'Assamese',
+      value: 'asm', text: 'Assamese',
     },
     {
-      key: 'ara', text: 'Avaric',
+      value: 'ara', text: 'Avaric',
     },
     {
-      key: 'ave', text: 'Avestan',
+      value: 'ave', text: 'Avestan',
     },
     {
-      key: 'aym', text: 'Aymara',
+      value: 'aym', text: 'Aymara',
     },
     {
-      key: 'aze', text: 'Azerbaijani',
+      value: 'aze', text: 'Azerbaijani',
     },
     {
-      key: 'bam', text: 'Bambara',
+      value: 'bam', text: 'Bambara',
     },
     {
-      key: 'bak', text: 'Bashkir',
+      value: 'bak', text: 'Bashkir',
     },
     {
-      key: 'eus', text: 'Basque',
+      value: 'eus', text: 'Basque',
     },
     {
-      key: 'bel', text: 'Belarusian',
+      value: 'bel', text: 'Belarusian',
     },
     {
-      key: 'ben', text: 'Bengali',
+      value: 'ben', text: 'Bengali',
     },
     {
-      key: 'bih', text: 'Bihari languages',
+      value: 'bih', text: 'Bihari languages',
     },
     {
-      key: 'bis', text: 'Bislama',
+      value: 'bis', text: 'Bislama',
     },
     {
-      key: 'bos', text: 'Bosnian',
+      value: 'bos', text: 'Bosnian',
     },
     {
-      key: 'bre', text: 'Breton',
+      value: 'bre', text: 'Breton',
     },
     {
-      key: 'bul', text: 'Bulgarian',
+      value: 'bul', text: 'Bulgarian',
     },
     {
-      key: 'mya', text: 'Burmese',
+      value: 'mya', text: 'Burmese',
     },
     {
-      key: 'cat', text: 'Catalan, Valencian',
+      value: 'cat', text: 'Catalan, Valencian',
     },
     {
-      key: 'cha', text: 'Chamorro',
+      value: 'cha', text: 'Chamorro',
     },
     {
-      key: 'che', text: 'Chechen',
+      value: 'che', text: 'Chechen',
     },
     {
-      key: 'nya', text: 'Chichewa, Chewa, Nyanja',
+      value: 'nya', text: 'Chichewa, Chewa, Nyanja',
     },
     {
-      key: 'chi', text: 'Chinese',
+      value: 'chi', text: 'Chinese',
     },
     {
-      key: 'chv', text: 'Chuvash',
+      value: 'chv', text: 'Chuvash',
     },
     {
-      key: 'cor', text: 'Cornish',
+      value: 'cor', text: 'Cornish',
     },
     {
-      key: 'cos', text: 'Corsican',
+      value: 'cos', text: 'Corsican',
     },
     {
-      key: 'cre', text: '	Cree',
+      value: 'cre', text: '	Cree',
     },
     {
-      key: 'hrv', text: 'Croatian',
+      value: 'hrv', text: 'Croatian',
     },
     {
-      key: 'cze', text: 'Czech',
+      value: 'cze', text: 'Czech',
     },
     {
-      key: 'dan', text: 'Danish',
+      value: 'dan', text: 'Danish',
     },
     {
-      key: 'div', text: 'Divehi, Dhivehi, Maldivian',
+      value: 'div', text: 'Divehi, Dhivehi, Maldivian',
     },
     {
-      key: 'nld', text: 'Dutch, Flemish',
+      value: 'nld', text: 'Dutch, Flemish',
     },
     {
-      key: 'dzo', text: 'Dzongkha',
+      value: 'dzo', text: 'Dzongkha',
     },
     {
-      key: 'eng', text: 'English',
+      value: 'eng', text: 'English',
     },
     {
-      key: 'epo', text: 'Esperanto',
+      value: 'epo', text: 'Esperanto',
     },
     {
-      key: 'est', text: 'Estonian',
+      value: 'est', text: 'Estonian',
     },
     {
-      key: 'ewe', text: 'Ewe',
+      value: 'ewe', text: 'Ewe',
     },
     {
-      key: 'fao', text: 'Faroese',
+      value: 'fao', text: 'Faroese',
     },
     {
-      key: 'fij', text: 'Fijian',
+      value: 'fij', text: 'Fijian',
     },
     {
-      key: 'fil', text: 'Filipino',
+      value: 'fil', text: 'Filipino',
     },
     {
-      key: 'fin', text: 'Finnish',
+      value: 'fin', text: 'Finnish',
     },
     {
-      key: 'fra', text: 'French',
+      value: 'fra', text: 'French',
     },
     {
-      key: 'ful', text: 'Fulah',
+      value: 'ful', text: 'Fulah',
     },
     {
-      key: 'glg', text: 'Galician',
+      value: 'glg', text: 'Galician',
     },
     {
-      key: 'kat', text: 'Georgian',
+      value: 'kat', text: 'Georgian',
     },
     {
-      key: 'deu', text: 'German',
+      value: 'deu', text: 'German',
     },
     {
-      key: 'ell', text: 'Greek',
+      value: 'ell', text: 'Greek',
     },
     {
-      key: 'grn', text: 'Guaraní',
+      value: 'grn', text: 'Guaraní',
     },
     {
-      key: 'guj', text: 'Gujarati',
+      value: 'guj', text: 'Gujarati',
     },
     {
-      key: 'hat', text: 'Haitian, Haitian Creole',
+      value: 'hat', text: 'Haitian, Haitian Creole',
     },
     {
-      key: 'hau', text: 'Hausa',
+      value: 'hau', text: 'Hausa',
     },
     {
-      key: 'heb', text: 'Hebrew',
+      value: 'heb', text: 'Hebrew',
     },
     {
-      key: 'her', text: 'Herero',
+      value: 'her', text: 'Herero',
     },
     {
-      key: 'hin', text: 'Hindi',
+      value: 'hin', text: 'Hindi',
     },
     {
-      key: 'hmo', text: 'Hiri Motu',
+      value: 'hmo', text: 'Hiri Motu',
     },
     {
-      key: 'hun', text: 'Hungarian',
+      value: 'hun', text: 'Hungarian',
     },
     {
-      key: 'ina', text: 'Interlingua',
+      value: 'ina', text: 'Interlingua',
     },
     {
-      key: 'ind', text: 'Indonesian',
+      value: 'ind', text: 'Indonesian',
     },
     {
-      key: 'ile', text: 'Interlingue',
+      value: 'ile', text: 'Interlingue',
     },
     {
-      key: 'gle', text: 'Irish',
+      value: 'gle', text: 'Irish',
     },
     {
-      key: 'ibo', text: 'Igbo',
+      value: 'ibo', text: 'Igbo',
     },
     {
-      key: 'ipk', text: 'Inupiaq',
+      value: 'ipk', text: 'Inupiaq',
     },
     {
-      key: 'ido', text: 'Ido',
+      value: 'ido', text: 'Ido',
     },
     {
-      key: 'isl', text: 'Icelandic',
+      value: 'isl', text: 'Icelandic',
     },
     {
-      key: 'ita', text: 'Italian',
+      value: 'ita', text: 'Italian',
     },
     {
-      key: 'iku', text: 'Inuktitut',
+      value: 'iku', text: 'Inuktitut',
     },
     {
-      key: 'jpn', text: 'Japanese',
+      value: 'jpn', text: 'Japanese',
     },
     {
-      key: 'jav', text: 'Javanese',
+      value: 'jav', text: 'Javanese',
     },
     {
-      key: 'kal', text: 'Kalaallisut, Greenlandic',
+      value: 'kal', text: 'Kalaallisut, Greenlandic',
     },
     {
-      key: 'kan', text: 'Kannada',
+      value: 'kan', text: 'Kannada',
     },
     {
-      key: 'kau', text: 'Kanuri',
+      value: 'kau', text: 'Kanuri',
     },
     {
-      key: 'kas', text: 'Kashmiri',
+      value: 'kas', text: 'Kashmiri',
     },
     {
-      key: 'kaz', text: 'Kazakh',
+      value: 'kaz', text: 'Kazakh',
     },
     {
-      key: 'khm', text: 'Central Khmer',
+      value: 'khm', text: 'Central Khmer',
     },
     {
-      key: 'kik', text: 'Kikuyu, Gikuyu',
+      value: 'kik', text: 'Kikuyu, Gikuyu',
     },
     {
-      key: 'kin', text: 'Kinyarwanda',
+      value: 'kin', text: 'Kinyarwanda',
     },
     {
-      key: 'kir', text: 'Kirghiz, Kyrgyz',
+      value: 'kir', text: 'Kirghiz, Kyrgyz',
     },
     {
-      key: 'kom', text: 'Komi',
+      value: 'kom', text: 'Komi',
     },
     {
-      key: 'kon', text: 'Kongo',
+      value: 'kon', text: 'Kongo',
     },
     {
-      key: 'kor', text: 'Korean',
+      value: 'kor', text: 'Korean',
     },
     {
-      key: 'kur', text: 'Kurdish',
+      value: 'kur', text: 'Kurdish',
     },
     {
-      key: 'kua', text: 'Kuanyama, Kwanyama',
+      value: 'kua', text: 'Kuanyama, Kwanyama',
     },
     {
-      key: 'lat', text: 'Latin',
+      value: 'lat', text: 'Latin',
     },
     {
-      key: 'ltz', text: 'Luxembourgish, Letzeburgesch',
+      value: 'ltz', text: 'Luxembourgish, Letzeburgesch',
     },
     {
-      key: 'lug', text: 'Ganda',
+      value: 'lug', text: 'Ganda',
     },
     {
-      key: 'lim', text: 'Limburgan, Limburger, Limburgish',
+      value: 'lim', text: 'Limburgan, Limburger, Limburgish',
     },
     {
-      key: 'lin', text: 'Lingala',
+      value: 'lin', text: 'Lingala',
     },
     {
-      key: 'lao', text: 'Lao',
+      value: 'lao', text: 'Lao',
     },
     {
-      key: 'lit', text: 'Lithuanian',
+      value: 'lit', text: 'Lithuanian',
     },
     {
-      key: 'lub', text: 'Luba-Katanga',
+      value: 'lub', text: 'Luba-Katanga',
     },
     {
-      key: 'lav', text: 'Latvian',
+      value: 'lav', text: 'Latvian',
     },
     {
-      key: 'glv', text: 'Manx',
+      value: 'glv', text: 'Manx',
     },
     {
-      key: 'mkd', text: 'Macedonian',
+      value: 'mkd', text: 'Macedonian',
     },
     {
-      key: 'mlg', text: 'Malagasy',
+      value: 'mlg', text: 'Malagasy',
     },
     {
-      key: 'msa', text: 'Malay',
+      value: 'msa', text: 'Malay',
     },
     {
-      key: 'mal', text: 'Malayalam',
+      value: 'mal', text: 'Malayalam',
     },
     {
-      key: 'mlt', text: 'Maltese',
+      value: 'mlt', text: 'Maltese',
     },
     {
-      key: 'mri', text: 'Maori',
+      value: 'mri', text: 'Maori',
     },
     {
-      key: 'mar', text: 'Marathi',
+      value: 'mar', text: 'Marathi',
     },
     {
-      key: 'mah', text: 'Marshallese',
+      value: 'mah', text: 'Marshallese',
     },
     {
-      key: 'mon', text: 'Mongolian',
+      value: 'mon', text: 'Mongolian',
     },
     {
-      key: 'nau', text: 'Nauru',
+      value: 'nau', text: 'Nauru',
     },
     {
-      key: 'nav', text: 'Navajo, Navaho',
+      value: 'nav', text: 'Navajo, Navaho',
     },
     {
-      key: 'nde', text: 'North Ndebele',
+      value: 'nde', text: 'North Ndebele',
     },
     {
-      key: 'nep', text: 'Nepali',
+      value: 'nep', text: 'Nepali',
     },
     {
-      key: 'ndo', text: 'Ndonga',
+      value: 'ndo', text: 'Ndonga',
     },
     {
-      key: 'nob', text: 'Norwegian Bokmål',
+      value: 'nob', text: 'Norwegian Bokmål',
     },
     {
-      key: 'nno', text: 'Norwegian Nynorsk',
+      value: 'nno', text: 'Norwegian Nynorsk',
     },
     {
-      key: 'nor', text: 'Norwegian',
+      value: 'nor', text: 'Norwegian',
     },
     {
-      key: 'iii', text: 'Sichuan Yi, Nuosu',
+      value: 'iii', text: 'Sichuan Yi, Nuosu',
     },
     {
-      key: 'nbl', text: 'South Ndebele',
+      value: 'nbl', text: 'South Ndebele',
     },
     {
-      key: 'oci', text: 'Occitan',
+      value: 'oci', text: 'Occitan',
     },
     {
-      key: 'oji', text: 'Ojibwa',
+      value: 'oji', text: 'Ojibwa',
     },
     {
-      key: 'chu', text: 'Church Slavonic',
+      value: 'chu', text: 'Church Slavonic',
     },
     {
-      key: 'orm', text: 'Oromo',
+      value: 'orm', text: 'Oromo',
     },
     {
-      key: 'ori', text: 'Oriya',
+      value: 'ori', text: 'Oriya',
     },
     {
-      key: 'oss', text: 'Ossetian, Ossetic',
+      value: 'oss', text: 'Ossetian, Ossetic',
     },
     {
-      key: 'pan', text: 'Panjabi, Punjabi',
+      value: 'pan', text: 'Panjabi, Punjabi',
     },
     {
-      key: 'pli', text: 'Pali',
+      value: 'pli', text: 'Pali',
     },
     {
-      key: 'fas', text: 'Persian',
+      value: 'fas', text: 'Persian',
     },
     {
-      key: 'pol', text: 'Polish',
+      value: 'pol', text: 'Polish',
     },
     {
-      key: 'pus', text: 'Pashto, Pushto',
+      value: 'pus', text: 'Pashto, Pushto',
     },
     {
-      key: 'por', text: 'Portuguese',
+      value: 'por', text: 'Portuguese',
     },
     {
-      key: 'que', text: 'Quechua',
+      value: 'que', text: 'Quechua',
     },
     {
-      key: 'roh', text: 'Romansh',
+      value: 'roh', text: 'Romansh',
     },
     {
-      key: 'run', text: 'Rundi',
+      value: 'run', text: 'Rundi',
     },
     {
-      key: 'ron', text: 'Romanian',
+      value: 'ron', text: 'Romanian',
     },
     {
-      key: 'rus', text: 'Russian',
+      value: 'rus', text: 'Russian',
     },
     {
-      key: 'san', text: 'Sanskrit',
+      value: 'san', text: 'Sanskrit',
     },
     {
-      key: 'srd', text: 'Sardinian',
+      value: 'srd', text: 'Sardinian',
     },
     {
-      key: 'snd', text: 'Sindhi',
+      value: 'snd', text: 'Sindhi',
     },
     {
-      key: 'sme', text: 'Northern Sami',
+      value: 'sme', text: 'Northern Sami',
     },
     {
-      key: 'smo', text: 'Samoan',
+      value: 'smo', text: 'Samoan',
     },
     {
-      key: 'sag', text: 'Sango',
+      value: 'sag', text: 'Sango',
     },
     {
-      key: 'srp', text: 'Serbian',
+      value: 'srp', text: 'Serbian',
     },
     {
-      key: 'gla', text: 'Gaelic, Scottish',
+      value: 'gla', text: 'Gaelic, Scottish',
     },
     {
-      key: 'sna', text: 'Shona',
+      value: 'sna', text: 'Shona',
     },
     {
-      key: 'sin', text: 'Sinhala, Sinhalese',
+      value: 'sin', text: 'Sinhala, Sinhalese',
     },
     {
-      key: 'slk', text: 'Slovak',
+      value: 'slk', text: 'Slovak',
     },
     {
-      key: 'slv', text: 'Slovenian',
+      value: 'slv', text: 'Slovenian',
     },
     {
-      key: 'som', text: 'Somali',
+      value: 'som', text: 'Somali',
     },
     {
-      key: 'sot', text: 'Southern Sotho',
+      value: 'sot', text: 'Southern Sotho',
     },
     {
-      key: 'spa', text: 'Spanish, Castilian',
+      value: 'spa', text: 'Spanish, Castilian',
     },
     {
-      key: 'sun', text: 'Sundanese',
+      value: 'sun', text: 'Sundanese',
     },
     {
-      key: 'swa', text: 'Swahili',
+      value: 'swa', text: 'Swahili',
     },
     {
-      key: 'ssw', text: 'Swati',
+      value: 'ssw', text: 'Swati',
     },
     {
-      key: 'swe', text: 'Swedish',
+      value: 'swe', text: 'Swedish',
     },
     {
-      key: 'tam', text: 'Tamil',
+      value: 'tam', text: 'Tamil',
     },
     {
-      key: 'tel', text: 'Telugu',
+      value: 'tel', text: 'Telugu',
     },
     {
-      key: 'tgk', text: 'Tajik',
+      value: 'tgk', text: 'Tajik',
     },
     {
-      key: 'tha', text: 'Thai',
+      value: 'tha', text: 'Thai',
     },
     {
-      key: 'tir', text: 'Tigrinya',
+      value: 'tir', text: 'Tigrinya',
     },
     {
-      key: 'bod', text: 'Tibetan',
+      value: 'bod', text: 'Tibetan',
     },
     {
-      key: 'tuk', text: 'Turkmen',
+      value: 'tuk', text: 'Turkmen',
     },
     {
-      key: 'tgl', text: 'Tagalog',
+      value: 'tgl', text: 'Tagalog',
     },
     {
-      key: 'tsn', text: 'Tswana',
+      value: 'tsn', text: 'Tswana',
     },
     {
-      key: 'ton', text: 'Tonga (Tonga Islands)',
+      value: 'ton', text: 'Tonga (Tonga Islands)',
     },
     {
-      key: 'tur', text: 'Turkish',
+      value: 'tur', text: 'Turkish',
     },
     {
-      key: 'tso', text: 'Tsonga',
+      value: 'tso', text: 'Tsonga',
     },
     {
-      key: 'tat', text: 'Tatar',
+      value: 'tat', text: 'Tatar',
     },
     {
-      key: 'twi', text: 'Twi',
+      value: 'twi', text: 'Twi',
     },
     {
-      key: 'tah', text: 'Tahitian',
+      value: 'tah', text: 'Tahitian',
     },
     {
-      key: 'uig', text: 'Uighur, Uyghur',
+      value: 'uig', text: 'Uighur, Uyghur',
     },
     {
-      key: 'ukr', text: 'Ukrainian',
+      value: 'ukr', text: 'Ukrainian',
     },
     {
-      key: 'urd', text: 'Urdu',
+      value: 'urd', text: 'Urdu',
     },
     {
-      key: 'uzb', text: 'Uzbek',
+      value: 'uzb', text: 'Uzbek',
     },
     {
-      key: 'ven', text: 'Venda',
+      value: 'ven', text: 'Venda',
     },
     {
-      key: 'vie', text: 'Vietnamese',
+      value: 'vie', text: 'Vietnamese',
     },
     {
-      key: 'vol', text: 'Volapük',
+      value: 'vol', text: 'Volapük',
     },
     {
-      key: 'wln', text: 'Walloon',
+      value: 'wln', text: 'Walloon',
     },
     {
-      key: 'cym', text: 'Welsh',
+      value: 'cym', text: 'Welsh',
     },
     {
-      key: 'wol', text: 'Wolof',
+      value: 'wol', text: 'Wolof',
     },
     {
-      key: 'fry', text: 'Western Frisian',
+      value: 'fry', text: 'Western Frisian',
     },
     {
-      key: 'xho', text: 'Xhosa',
+      value: 'xho', text: 'Xhosa',
     },
     {
-      key: 'yid', text: 'Yiddish',
+      value: 'yid', text: 'Yiddish',
     },
     {
-      key: 'yor', text: 'Yoruba',
+      value: 'yor', text: 'Yoruba',
     },
     {
-      key: 'zha', text: 'Zhuang, Chuang',
+      value: 'zha', text: 'Zhuang, Chuang',
     },
     {
-      key: 'zul', text: 'Zulu',
+      value: 'zul', text: 'Zulu',
     },
 
   ],

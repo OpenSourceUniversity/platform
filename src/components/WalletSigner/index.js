f955a49dbd7910bcebdc25c730497d238549b0e4
Dobromir Kovachev <dobromir.kovachev@os.university>
2018-06-03 19:18:59 +0300

    Code style fixes for unlocker and signer

--- src/components/WalletSigner/index.js
+++ src/components/WalletSigner/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Button, Icon } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
 
 export default class WalletSigner extends React.Component {
-
   render() {
     const { ...rest } = this.props;
     return (
-        <Button primary { ...rest } >
-            { this.props.buttonCaption }
-        </Button>
+      <Button primary {...rest} >
+        { this.props.buttonCaption }
+      </Button>
     );
   }
 }

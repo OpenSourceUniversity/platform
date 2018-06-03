bfd90409c4af5d15c8067b2c66773fa64dcd00d2
Dobromir Kovachev <dobromir.kovachev@os.university>
2018-06-04 02:59:07 +0300

    WIP searsh component

---
+++ src/components/Search/index.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import { Input, Dropdown } from 'semantic-ui-react';
+//
+//
+// export default class Search extends React.Component {
+//
+//
+//   // render() {
+//   //   return (
+//   //     // <Form.Dropdown
+//   //     //   label="Search"
+//   //     //   placeholder="Search by keyword..."
+//   //     //   fluid
+//   //     //   multiple
+//   //     //   search
+//   //     //   selection
+//   //     // />
+//   //   );
+//   // }
+// }

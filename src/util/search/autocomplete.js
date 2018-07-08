7b658184a0deba32fce9f86c7180e1f349a15db9
Jordan Jambazov <jordan.jambazov@os.university>
2018-07-08 15:35:07 +0300

    Authenticate to BDN when autocompleting

--- src/util/search/autocomplete.js
+++ src/util/search/autocomplete.js
@@ -1,10 +1,17 @@
+import store from '../../store';
+
+
 export default function autocomplete(query) {
   return function action(dispatch) {
     dispatch({
       type: 'AUTOCOMPLETE_REQUEST',
     });
     const url = `http://localhost:8000/api/v1/courses/autocomplete/?q=${query}`;
-    return fetch(url)
+    const headers = new Headers({
+      'Auth-Signature': store.getState().auth.signedAddress,
+      'Auth-Eth-Address': store.getState().auth.address.slice(2),
+    });
+    return fetch(url, { headers })
       .then(response => response.json().then(body => ({ response, body })))
       .then(({ response, body }) => {
         if (!response.ok) {

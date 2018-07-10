9fae8e2eac065a22b56e3c8402ad3e49d4566c43
Dobromir Kovachev <dobromir.kovachev@os.university>
2018-07-10 13:33:59 +0300

    Fix props issue in container/CreateProfile

--- src/containers/CreateProfile/index.js
+++ src/containers/CreateProfile/index.js
@@ -4,13 +4,11 @@ import ChooseAccount from 'components/ChooseAccount';
 
 
 export default class CreateAccount extends React.Component {
-<<<<<<< HEAD
+
   constructor(props) {
     super(props);
   }
 
-=======
->>>>>>> 511a26daa0bf8a02766dd8c732f092874c788905
   render() {
     return (
       <div className="course">

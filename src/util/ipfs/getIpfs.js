d37d4aee70ad3539f3b1aa7e89ce964b0ccf8076
Dobromir Kovachev <dobromir.kovachev@os.university>
2018-08-02 12:02:57 +0300

    WIP ipfs instance

--- src/util/ipfs/getIpfs.js
+++ src/util/ipfs/getIpfs.js
@@ -1,25 +1,32 @@
 
-import ipfs from 'ipfs-api';
+const IPFS = require('ipfs-api');
 import store from '../../store';
 import Config from '../../config';
 
 export const IPFS_INITIALIZED = 'IPFS_INITIALIZED';
+
 function IPFSinitialized(results) {
-  return {
-    type: IPFS_INITIALIZED,
-    payload: results,
+  return function action(dispatch) {
+    dispatch({
+      type: 'IPFS_INITIALIZED',
+      payload: {
+        results,
+      },
+    });
   };
 }
 
 const getIpfs = new Promise(((resolve, reject) => {
   const { host, port, protocol } = Config.ipfs;
-  const ipfs = new ipfs(host, port, protocol);
+  const ipfs = new IPFS(host, port, protocol);
 
   const results = {
     IPFSinstance: ipfs,
   };
 
   resolve(store.dispatch(IPFSinitialized(results)));
+
 }));
+
 
 export default getIpfs;

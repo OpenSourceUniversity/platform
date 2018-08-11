d99326a5fbfe600814f705f73a7901c2b9c41951
Dobromir Kovachev <dobromir.kovachev@os.university>
2018-08-11 14:16:13 +0300

    WIP verify certificate and upload metaJson file in IPFS

--- src/util/verification/verifyCertificate.js
+++ src/util/verification/verifyCertificate.js
@@ -6,19 +6,27 @@ import { initWalletUnlocker } from '../../util/auth/walletUnlocker';
 
 const { bdnUrl } = Config.network;
 
-
 export default function verifyCertificate(certificateData) {
   return function action(dispatch) {
+    dispatch({
+      type: 'UPDATE_CERTIFICATE_REQUEST',
+    });
     const ipfs = store.getState().ipfs.IPFSinstance;
     const metaJson = JSON.stringify(certificateData);
     const metaJsonBuffer = Buffer.from(metaJson);
     ipfs.add(metaJsonBuffer, (err, ipfsHash) => {
-      initWalletUnlocker((wallet) => {
+      dispatch({
+      type: 'IPFS_GET_SUCCESS',
+      payload: {
+        ipfsHash: ipfsHash[0].hash,
+      },
+      });
+      dispatch(initWalletUnlocker((wallet) => {
         dispatch(updateCertificate(certificateData, () => {
           const url = `${bdnUrl}api/v1/certificates/get_certificates_by_academy/`;
           dispatch(fetchCertificates(url));
         }));
-      });
+      }));
     });
   };
 }

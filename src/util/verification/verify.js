088bc799460ba286adb069126c7c57f84971a79b
Jordan Jambazov <jordan.jambazov@os.university>
2018-08-23 17:48:06 +0300

    Update stuff stored in meta JSON

--- src/util/verification/verify.js
+++ src/util/verification/verify.js
@@ -5,21 +5,15 @@ import fetchVerifications from './fetchVerifications';
 
 export default function verify(verification) {
   return function action(dispatch) {
-    // TODO: do not dispatch this event. Instead dispatch something more
-    // related to the verification functionality, e.g. VERIFICATION_REQUEST
     dispatch({
       type: 'VERIFY_REQUEST',
     });
     const ipfs = store.getState().ipfs.IPFSinstance;
     const metaJson = verification;
     metaJson.previous_state = metaJson.state;
-    const verificationID = verification.id;
+    const verificationId = verification.id;
     delete metaJson.state;
-    delete metaJson.id;
-    delete metaJson.certificate.id;
-    metaJson.granted_to = verification.certificate.learner_eth_address;
-    metaJson.verifier = verification.certificate.provider.eth_address;
-    console.log(metaJson);
+
     const metaJsonBuffer = Buffer.from(JSON.stringify(metaJson));
     ipfs.add(metaJsonBuffer, (err, ipfsHashFull) => {
       const ipfsHash = ipfsHashFull[0].hash;
@@ -34,7 +28,7 @@ export default function verify(verification) {
           return;
         }
         // Store the updated data on BDN
-        dispatch(setPendingVerification(verificationID, () => {
+        dispatch(setPendingVerification(verificationId, () => {
           dispatch(fetchVerifications());
         }));
       }));

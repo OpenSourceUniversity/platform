ef102a6c60998b80e8ba1f074c9914bc76f84679
Momchil Jambazov <momchil.jambazov@os.university>
2018-10-16 13:08:27 +0300

    Learners and Deposit page update

--- src/containers/Deposit/index.js
+++ src/containers/Deposit/index.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import React from 'react';
 import QRCode from 'qrcode.react';
-import { Segment, Container, Grid, Card, Image, Button, Icon, Header, Divider, Statistic, Table, Input, Form, Dropdown, Dimmer, Message, Breadcrumb } from 'semantic-ui-react';
+import { Tab, Segment, Container, Grid, Card, Image, Button, Icon, Header, Divider, Statistic, Table, Input, Form, Dropdown, Dimmer, Message, Breadcrumb } from 'semantic-ui-react';
 import TransactionHistoryItem from 'components/TransactionHistoryItem';
 import { initWalletUnlocker } from '../../util/auth/walletUnlocker';
 import getBalances from '../../util/web3/getBalances';
@@ -11,6 +11,11 @@ import Config from '../../config';
 import GasPriceExtension from '../../components/GasPriceExtension';
 import { withdraw, resetWithdrawProps } from './actions';
 import getWithdrawTransactions from '../../util/withdraw/getWithdrawTransactions';
+
+const panes = [
+  { menuItem: 'Deposit', render: () => <Tab.Pane attached='bottom'> Deposit Content Here </Tab.Pane> },
+  { menuItem: 'Withdraw', render: () => <Tab.Pane attached='bottom'> Withdraw Content Here </Tab.Pane> },
+]
 
 class Deposit extends React.Component {
   constructor(props) {
@@ -123,111 +128,104 @@ class Deposit extends React.Component {
           Deposit/Withdraw
         </Header>
         <Divider />
-        <Grid divided="vertically">
+        <Grid>
           <Grid.Row>
-            <Grid.Column mobile={16} tablet={16} computer={10}>
-              <Grid>
-                <Grid.Row>
-                  <Grid.Column mobile={16} tablet={8} computer={8}>
-                    <Card fluid>
-                      <Card.Content>
-                        <Image floated="right" style={{ borderRadius: '50%' }} size="mini" src={this.getAvatar()} />
-                        <Card.Header>
-                          DEPOSIT
-                        </Card.Header>
-                        <Card.Meta>
-                          Send EDU Tokens or ETH to the address below.
-                        </Card.Meta>
-                        <Card.Meta style={{ textAlign: 'center', paddingTop: '1em', paddingBottom: '1em' }}>
-                          <QRCode value={this.props.address} />
-                        </Card.Meta>
-                        <Card.Description>
-                          <Form className="attached fluid segment">
-                            <Form.Input id="WalletAddress" style={{ fontSize: '0.9em' }} readOnly label="ETH/EDU Deposit Address:" fluid value={this.props.address} />
-                          </Form>
-                        </Card.Description>
-                      </Card.Content>
-                      <Card.Content extra>
-                        <div>
-                          <Button basic color="grey" onClick={this.copyAddress}><Icon name="copy" /> Copy Address</Button>
-                          <Button basic color="grey" onClick={this.showPrivateKey}>
-                            <Icon name="copy" />
-                            Show Private Key
-                          </Button>
-                        </div>
-                      </Card.Content>
-                    </Card>
-                  </Grid.Column>
-                  <Grid.Column mobile={16} tablet={8} computer={8}>
-                    <Card fluid>
-                      <Card.Content>
-                        <Image floated="right" style={{ borderRadius: '50%' }} size="mini" src={this.getAvatar()} />
-                        <Card.Header>
-                          WITHDRAW
-                        </Card.Header>
-                        <Card.Meta>
-                          Select
-                        </Card.Meta>
-                        <Card.Description>
-                          <Form
-                            id="withdrawForm"
-                            className="attached fluid segment"
-                            onSubmit={
-                              (event) => {
-                                this.submitWithdraw(event, this);
-                              }
-                            }
-                          >
-                            <Form.Input
-                              list="wallets"
-                              name="wallet"
-                              label="Choose withdraw wallet:"
-                              fluid
-                              onChange={
-                                (event, { name, value }) => { this.setState({ [name]: value }); }
-                              }
-                              placeholder="Enter ETH address:"
-                            />
-                            <Input
-                              fluid
-                              name="amount"
-                              onChange={
-                                (event, { name, value }) => { this.setState({ [name]: value }); }
-                              }
-                              label={
-                                <Dropdown
-                                  defaultValue="edu"
-                                  onChange={(event, data) => { this.onCoinChange(event, data); }}
-                                  options={
-                                    [
-                                      { key: 'edu', text: 'EDU', value: 'edu' },
-                                      { key: 'eth', text: 'ETH', value: 'eth' },
-                                    ]}
-                                />
-                              }
-                              labelPosition="right"
-                              placeholder="0.0000"
-                            />
-                          </Form>
-                        </Card.Description>
-                      </Card.Content>
-                      <Card.Content extra>
-                        <Button
-                          form="withdrawForm"
-                          type="submit"
-                          primary
-                          fluid
-                          disabled={!this.state.amount || !this.state.wallet}
-                        >
-                          Submit Withdraw
-                        </Button>
-                      </Card.Content>
-                    </Card>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
+            <Grid.Column mobile={16} tablet={8} computer={8}>
+              <Tab menu={{ attached: 'top', size: 'huge' }} panes={panes} />
+              <Card fluid>
+                <Card.Content>
+                  <Image floated="right" style={{ borderRadius: '50%' }} size="mini" src={this.getAvatar()} />
+                  <Card.Header>
+                    DEPOSIT
+                  </Card.Header>
+                  <Card.Meta>
+                    Send EDU Tokens or ETH to the address below.
+                  </Card.Meta>
+                  <Card.Meta style={{ textAlign: 'center', paddingTop: '1em', paddingBottom: '1em' }}>
+                    <QRCode value={this.props.address} />
+                  </Card.Meta>
+                  <Card.Description>
+                    <Form className="attached fluid segment">
+                      <Form.Input id="WalletAddress" style={{ fontSize: '0.9em' }} readOnly label="ETH/EDU Deposit Address:" fluid value={this.props.address} />
+                    </Form>
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <div>
+                    <Button basic color="grey" onClick={this.copyAddress}><Icon name="copy" /> Copy Address</Button>
+                    <Button basic color="grey" onClick={this.showPrivateKey}>
+                      <Icon name="copy" />
+                      Show Private Key
+                    </Button>
+                  </div>
+                </Card.Content>
+              </Card>
+              <Card fluid>
+                <Card.Content>
+                  <Image floated="right" style={{ borderRadius: '50%' }} size="mini" src={this.getAvatar()} />
+                  <Card.Header>
+                    WITHDRAW
+                  </Card.Header>
+                  <Card.Meta>
+                    Select
+                  </Card.Meta>
+                  <Card.Description>
+                    <Form
+                      id="withdrawForm"
+                      className="attached fluid segment"
+                      onSubmit={
+                        (event) => {
+                          this.submitWithdraw(event, this);
+                        }
+                      }
+                    >
+                      <Form.Input
+                        list="wallets"
+                        name="wallet"
+                        label="Choose withdraw wallet:"
+                        fluid
+                        onChange={
+                          (event, { name, value }) => { this.setState({ [name]: value }); }
+                        }
+                        placeholder="Enter ETH address:"
+                      />
+                      <Input
+                        fluid
+                        name="amount"
+                        onChange={
+                          (event, { name, value }) => { this.setState({ [name]: value }); }
+                        }
+                        label={
+                          <Dropdown
+                            defaultValue="edu"
+                            onChange={(event, data) => { this.onCoinChange(event, data); }}
+                            options={
+                              [
+                                { key: 'edu', text: 'EDU', value: 'edu' },
+                                { key: 'eth', text: 'ETH', value: 'eth' },
+                              ]}
+                          />
+                        }
+                        labelPosition="right"
+                        placeholder="0.0000"
+                      />
+                    </Form>
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <Button
+                    form="withdrawForm"
+                    type="submit"
+                    primary
+                    fluid
+                    disabled={!this.state.amount || !this.state.wallet}
+                  >
+                    Submit Withdraw
+                  </Button>
+                </Card.Content>
+              </Card>
             </Grid.Column>
-            <Grid.Column tablet={16} computer={6}>
+            <Grid.Column mobile={16} tablet={8} computer={8}>
               <Segment>
                 <Statistic size="tiny" color="orange">
                   <Statistic.Value>
